3ec09835f332cf408e35ae16234bf1963f63339b
Aman Aggarwal <amanaggarwal014@gmail.com>
2021-06-24 00:55:25 +0530

    Added speech to text functionality in Main activity so that users can use their voice to input name

--- app/src/main/java/com/amanaggarwal1/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/amanaggarwal1/birthdaygreet/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.amanaggarwal1.birthdaygreet
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CODE_STT = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,12 +29,41 @@ class MainActivity : AppCompatActivity() {
         if(name.isEmpty()){
             Toast.makeText(this, "Please enter a name", Toast.LENGTH_SHORT).show()
             return
-        }else if(name.length > 40){
+        }else if(name.length > 60){
             Toast.makeText(this, "Please enter a shorter name", Toast.LENGTH_SHORT).show()
             return
         }
         intent = Intent(this, BirthdayGreetActivity::class.java)
         intent.putExtra(BirthdayGreetActivity.NAME_EXTRA , name)
         startActivity(intent)
+    }
+
+    fun startSpeechToText(view: View) {
+
+        val sttIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        sttIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now!")
+        try {
+            startActivityForResult(sttIntent, REQUEST_CODE_STT)
+        } catch (e: ActivityNotFoundException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Your device does not support STT.", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_CODE_STT -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    if (!result.isNullOrEmpty()) {
+                        val recognizedText = result[0]
+                            personNameET.setText(recognizedText)
+                    }
+                }
+            }
+        }
     }
 }
